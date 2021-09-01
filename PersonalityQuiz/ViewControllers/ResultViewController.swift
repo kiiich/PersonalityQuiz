7c978cf77acbf3461222d8fe161b032595bd00af
KIIICH <KIIICH>
2021-09-01 19:38:03 +0300

    Programming logic

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var animalDescription: UILabel!
+    
+    var answers: [Answer] = []
+    
     // 1. Передать сюда массив с ответами
     // 2. Определить наиболее часто встречающийся тип животного
     // 3. Отобразить результат в соответствии с этим животным
@@ -16,6 +21,33 @@ class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+}
+
+extension ResultViewController {
+    
+    private func updateUI() {
+        
+        var animals: [Animal: Int] = [:]
+        
+        answers.forEach { answer in
+            if let currentAnimal = animals[answer.animal] {
+                animals[answer.animal] = currentAnimal + 1
+            } else {
+                animals[answer.animal] = 1
+            }
+        }
+        
+        let maxAnimal = animals.max { $0.value < $1.value }
+        
+        guard let currentAnimal = maxAnimal?.key else {
+            return
+        }
+        
+        header.text = "Вы - \(currentAnimal.rawValue)"
+        animalDescription.text = currentAnimal.definition
+        
+    }
+    
 }
