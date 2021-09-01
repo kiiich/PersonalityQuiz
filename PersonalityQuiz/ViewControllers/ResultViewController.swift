b3f7bf828b4e9993329f90766443fdbe162dc8a1
KIIICH <KIIICH>
2021-09-01 19:45:18 +0300

    Hided back button

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -13,14 +13,10 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var animalDescription: UILabel!
     
     var answers: [Answer] = []
-    
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответствии с этим животным
-    // 4. Избавиться от кнопки возврата на предыдущий экран
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.setHidesBackButton(true, animated: false)
         updateUI()
     }
 }
